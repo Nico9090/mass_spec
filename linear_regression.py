6780844961887ef69689895eccf50ec2ee4e7657
Nico9090 <161892855+Nico9090@users.noreply.github.com>
2025-01-03 13:09:55 -0500

    Update linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -1,59 +1,36 @@
 #!/usr/bin/env python3
-#Obtain values from a dataset
-x,y=[],[]
-with open('xpoints.txt','r')as f:
-        for line in f:
-                x.append(int(line[0:-1]))
-with open('ypoints.txt','r')as f:
-        for line in f:
-                y.append(int(line[0:-1]))
-#Linear regression test
-
-#1) Find the y-int
-alpha=[]
-for i in range(len(x)):
-        if x[i]==0:
-                alpha.append(y[i])
-#2) Find the slope
-
-s=(y[-1]-y[0])//(x[-1]-x[0])
-
-#2) Find sum of residual squares
-rsqs=[]
-for i in range(0,len(y)):
-        rsqs.append(y[i]-alpha[0]-s*x[i])
-sum_rsqs=0
-for i in rsqs:
-        sum_rsqs+=i
-
-#Finding alpha
-
-#1)Find sum of y values
-sum_y=0
-for i in range(len(y)):
-        sum_y+=y[i]
-
-#2) Find sum of x values
-sum_x=0
-for i in range(len(x)):
-        sum_x+=x[i]
-#2)Find B
-bup=[]
-bdown=[]
-for i in range(len(y)):
-        bup.append((x[i]-sum_x)*(y[i]-sum_y))
-        bdown.append((x[i]-sum_x)**2)
-sum_bup=0
-sum_bdown=0
-for i in range(len(bup)):
-        sum_bup+=i
-for i in range(len(bdown)):
-        sum_bdown+=i
-b=sum_bup/sum_bdown
-print(b)
-#4)Find a
-a=sum_y-(b*sum_x)
-print(a)
-print(sum_y)
-print(sum_x)
-
+import re
+import numpy as np
+class Supervised_ML:
+    def __init__(self,data_file):
+            self.data=data_file
+    def linear_R(data_file):
+            x,y=[],[]
+            with open(data_file,'r')as f:
+                for line in f:
+                        if not re.match(r'^\d',line):
+                              continue
+                        else:
+                             points=line.strip().split("|")
+                             x.append(float(points[0]))
+                             y.append(float(points[-1])) 
+            mean_x,mean_y=np.mean(x),np.mean(y)
+            stdev_x,stdev_y=np.std(x),np.std(y)
+            cor=np.corrcoef(x,y)[0,1] #source:datacamp
+            sl=(cor*stdev_y)/(stdev_x)
+            y_int=mean_y-sl*mean_x
+            y_pred=[]
+            y_pred.append(sl*x+y_int)
+            return f"slope : {sl}, y_intercept : {y_int}"
+    def logistic_R(data_file):
+          x,y=[],[]
+          with open(data_file,'r')as f:
+                for line in f:
+                        if not re.match(r'^\d',line):
+                              continue
+                        else:
+                             points=line.strip().split("|")
+                             x.append(float(points[0]))
+                             y.append(float(points[-1])) 
+sample_data=Supervised_ML
+print(sample_data.linear_R("sample_file.txt"))
