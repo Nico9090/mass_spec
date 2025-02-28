c9f6e5880580b0f8b4b637537d48a3a02df55fde
Nico9090 <161892855+Nico9090@users.noreply.github.com>
2025-02-28 16:16:23 -0500

    Create PCA.R

---
+++ PCA.R
@@ -0,0 +1,4 @@
+sample_data<-data.frame(Dept=c("R&D","Human Resources","IT","Manufacturing"),
+                        Costs=c(1000000.273,4000.299,90230.21,1801200),
+                        Earnings=c(78990.12,728333.902,1020302.0,8839910.23))
+sample_data
