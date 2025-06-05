9b9e77cc73ec71ffa161b1a0aa0fbc031eb554b3
Nico9090 <161892855+Nico9090@users.noreply.github.com>
2025-06-05 14:34:06 -0400

    Create ms_prot.sh

---
+++ ms_prot.sh
@@ -0,0 +1,6 @@
+java -jar /path/to/programs/MSGFPlus.jar \
+        -s /path/to/mass_spec/JUN3659-S03-A-C7_cilia_BIOID2_DLG1KO_1.mzML \
+        -o cilia_BIOID2_DLG1KO_1.mzid \
+        -d /path/to/mass_spec/mouse_canon.fasta \
+        -t 20ppm \
+        -m 0
